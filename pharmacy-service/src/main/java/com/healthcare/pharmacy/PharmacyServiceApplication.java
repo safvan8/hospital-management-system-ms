3b1cb64c1eca9fab4d5d37d5b38482feed803edd
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-17 00:53:09 +0530

    PharmacyService - Initial Commit

---
+++ pharmacy-service/src/main/java/com/healthcare/pharmacy/PharmacyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyServiceApplication.class, args);
+	}
+
+}
